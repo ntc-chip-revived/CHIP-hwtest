8af94bee378d0ad80b3daf98ba745d0b93cf8e11
computermouth <computermouth@crunchbangplusplus.org>
2015-11-18 02:00:31 +0000

    Version control for builddeb script

--- builddeb.sh
+++ builddeb.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cd chip-hwtest*
-dpkg-buildpackage
+echo -e "\nIt looks like the last version built was 0."$(cat l_version)".."
+echo -e "\nWould you like to increment it to 0."$((cat l_version)+1)"?"
+
+#cd chip-hwtest*
+#dpkg-buildpackage
