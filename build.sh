2b0c94af1e2520494d0598e732f0eec47e18ec81
computermouth <computermouth@crunchbangplusplus.org>
2015-11-18 01:17:10 +0000

    Fixed directories and script for non-deb install

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+
+cd chip-hwtest*
+dpkg-buildpackage
--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 SRC_DIR=$
-sudo cp hwtest.sh /usr/bin/hwtest
-sudo cp power.sh /usr/bin/
-sudo cp battery.sh /usr/bin/
-sudo mkdir -p /usr/lib/hwtest
-sudo cp *.txt /usr/lib/hwtest
+#sudo cp hwtest.sh /usr/bin/hwtest
+#sudo cp power.sh /usr/bin/
+#sudo cp battery.sh /usr/bin/
+#sudo mkdir -p /usr/lib/hwtest
+#sudo cp *.txt /usr/lib/hwtest
+
+sudo cp -R chip-hwtest/bin/* /usr/bin/
+sudo cp -R chip-hwtest/lib/* /usr/lib/
