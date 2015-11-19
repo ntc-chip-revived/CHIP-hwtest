aed0d7fa62cfe53d69567c5b8b0278c1791ae5ff
Alex Kaplan <kaplan2539@gmail.com>
2015-11-19 14:20:24 -0800

    install stress on debian
    Signed-off-by: Alex Kaplan <kaplan2539@gmail.com>

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@ sudo cp power.sh /usr/bin/
 sudo cp battery.sh /usr/bin/
 sudo mkdir -p /usr/lib/hwtest
 sudo cp *.txt /usr/lib/hwtest
+
+if [[ "$(cat /etc/os-release |grep ID_LIKE)" == "ID_LIKE=debian" ]];
+then
+  sudo apt-get install stress
+fi
