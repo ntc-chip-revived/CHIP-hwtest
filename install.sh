4c244239355634ee6c4ab24371fa0bf6a69b5b3d
Alex Kaplan <kaplan2539@gmail.com>
2015-11-19 14:23:10 -0800

    [[ -> [
    Signed-off-by: Alex Kaplan <kaplan2539@gmail.com>

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo cp battery.sh /usr/bin/
 sudo mkdir -p /usr/lib/hwtest
 sudo cp *.txt /usr/lib/hwtest
 
-if [[ "$(cat /etc/os-release |grep ID_LIKE)" == "ID_LIKE=debian" ]];
+if [ "$(cat /etc/os-release |grep ID_LIKE)" == "ID_LIKE=debian" ];
 then
   sudo apt-get install stress
 fi
