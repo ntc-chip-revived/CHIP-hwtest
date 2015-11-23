3ebdfd6b5287bc89a27b8a340c06f198ab1b3916
computermouth <computermouth@crunchbangplusplus.org>
2015-11-23 19:35:48 +0000

    Removed unnecessary files.

--- builddeb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo -e "\nIt looks like the last version built was 0."$(cat l_version)".."
-echo -e "\nWould you like to increment it to 0."$((cat l_version)+1)"?"
-
-#cd chip-hwtest*
-#dpkg-buildpackage
