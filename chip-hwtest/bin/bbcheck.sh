c18fda24e95bdac62be3541bdc4de62b4987c2a3
Alex Kaplan <kaplan2539@gmail.com>
2016-02-16 20:43:41 +0000

    ak/CHIP-348

--- chip-hwtest/bin/bbcheck.sh
+++ chip-hwtest/bin/bbcheck.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-echo nanddump --bb=dumpbad -s 0x1FEC00000 -l 0x400000 -f /tmp/dump1FEC /dev/mtd4
-nanddump --bb=dumpbad -s 0x1FEC00000 -l 0x400000 -f /tmp/dump1FEC /dev/mtd4
-echo nanddump --bb=dumpbad -s 0x1FE800000 -l 0x400000 -f /tmp/dump1FE8 /dev/mtd4
-nanddump --bb=dumpbad -s 0x1FE800000 -l 0x400000 -f /tmp/dump1FE8 /dev/mtd4
-echo nanddump --bb=dumpbad -s 0x1FE400000 -l 0x400000 -f /tmp/dump1FE4 /dev/mtd4
-nanddump --bb=dumpbad -s 0x1FE400000 -l 0x400000 -f /tmp/dump1FE4 /dev/mtd4
-echo nanddump --bb=dumpbad -s 0x1FE000000 -l 0x400000 -f /tmp/dump1FE0 /dev/mtd4
-nanddump --bb=dumpbad -s 0x1FE000000 -l 0x400000 -f /tmp/dump1FE0 /dev/mtd4
-
-md5sum /tmp/dump1FEC /tmp/dump1FE8 /tmp/dump1FE4 /tmp/dump1FE0
+nanddump --bb=dumpbad -s 0x1FE000000 -l 0x400000 -f /dev/null /dev/mtd4 2>&1 | bbcheck.awk
